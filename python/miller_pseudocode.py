dae1310051170f1b13908df9c994afe490b23ed7
Jonathan Wang <jonathanpwang@gmail.com>
2022-02-11 18:41:53 -0800

    filled in miller_pseudocode. included a jupyter notebook with random test stuff.

--- python/miller_pseudocode.py
+++ python/miller_pseudocode.py
@@ -1,30 +1,34 @@
 import random
-q  = # a prime
-r = # prime divisor of the size of E[F_q]. assume r doesn't divide q-1
-bin_rep = [] # binary representation of r, without the leading 1
-k = # minimal k with r | q^k - 1
-phi = # distortion map sending P to a linearly independent phi(P)
+import curve
 
-def tate_pairing(P, Q):
+def tate_pairing(P, Q, k, r, phi, S):
     """
-    evaluate the tate pairing at points P, Q
+    evaluate the optimized Tate pairing at points P, Q
     returns an element of F_r
+    
+    k is embedding degree
+    r is prime divisor of size of E[F_q]. assume r doesn't divide q-1
+    phi = distortion map sending P to a linearly independent phi(P)
+    S is a random point not in [P, -P, Q, -Q, 0]
     """
+    # it seems like for optimized Tate perhaps the random point S can just equal P?
     Q1 = phi(Q)
     return exp(e(P, Q1), (q**k - 1) // r)
 
-def e(P, Q):
+# k is never explicitly needed below because we can use internal operators in FQk 
+
+def e(P, Q, r, S):
     """
-    evaluate a bilinear pairing e with 
-    e(aP, bQ) = e(P, Q)^{ab}
+    evaluate the Tate pairing at points P, Q which are r-torsion
+    S is a random point not in [P, -P, Q, -Q, 0]
+    
     returns an element of F_q^k
-    """
-    S = random()
-    f_P1 = miller(P, Q+S)
-    f_P2 = miller(P, S)
+    """ 
+    f_P1 = miller(P, add(Q,S), r)
+    f_P2 = miller(P, S, r)
     return div(f_P1, f_P2)
 
-def miller(P, Q):
+def miller(P, Q, r):
     """
     use miller's algorithm to construct a rational function 
     with r zeros at P, r poles at 0, and then evaluate the 
@@ -33,12 +37,13 @@ def miller(P, Q):
     """
     f = 1
     point = P
-    for i in range(len(bin_rep)):
+    while r:
         f = mul(mul(f, f), div(interpolate(point, point, Q), vertical(add(point, point), Q)))
         point = add(point, point)
-        if bin_rep[i] == 1:
+        if 1 & r:
             f = div(mul(f, interpolate(point, P, Q)), vertical(add(point, P), Q))
             point = add(point, P)
+        r //= 2
     return f
 
 
@@ -46,39 +51,46 @@ def exp(a, n):
     """
     compute a^n in Fq^k
     """
-    return
+    a_pow = a
+    while n:
+        if 1 & n:
+            res = res*a_pow if res else a_pow
+        n //= 2
+        a_pow *= a        
+    return res
 
 def mul(a, b):
     """
     multiply two elements of F_q^k
     """
-    return 
+    return a * b 
 
 def add(P, Q):
     """
     add two points on the elliptic curve. 
     done over F_q^k
     """
-    return 
+    return curve.add(P, Q)
 
 def div(a, b):
     """
     divide two elements of F_q^k
     """
-    return 
+    return a / b
 
 def interpolate(P1, P2, Q):
     """
     construct a linear function through P1, P2; evaluate it at Q. 
     done over F_q^k
     """
-    return 
+    lamb = (P2[1]-P1[1]) / (P2[0]-P1[0])
+    return lamb * (P1[0]-Q[0]) - (P1[1]-Q[1]) 
 
 def vertical(P, Q):
     """
     construct a vertical line through P; evaluate it at Q. 
     done over F_q^k
     """
-    return 
+    return Q[0] - P[0]
 
 # TODO: finite field arithmetic + elliptic curves
