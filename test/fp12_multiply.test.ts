345a8ab036cb6d2ea524a46a9783822a4919e74f
Vincent Huang <vincenthuang75025@yahoo.com>
2022-03-03 13:49:44 -0500

    Write some fp12 invert test cases

--- test/fp12_multiply.test.ts
+++ test/fp12_multiply.test.ts
@@ -196,17 +196,33 @@ describe("Fp12Invert n = 4, k = 2", function() {
     });
 
     // a0, a1, b0, b1, p, c0, c1
-    var test_cases: Array<number> = [0];
+    var test_cases: Array<number> = [2];
 
     var test_field_invert_42 = function (x: number) {
         var zero: bigint[] = bigint_to_array(4, 2, 0n);
-        let one: bigint[] = bigint_to_array(4, 2, 0n);
-        let minus_one: bigint[] = bigint_to_array(4, 2, 16n);
+        let one: bigint[] = bigint_to_array(4, 2, 1n);
+        let three: bigint[] = bigint_to_array(4, 2, 3n);
+        let four: bigint[] = bigint_to_array(4, 2, 4n);
+        let seven: bigint[] = bigint_to_array(4, 2, 7n);
+        let eight: bigint[] = bigint_to_array(4, 2, 8n);
+        let nine: bigint[] = bigint_to_array(4, 2, 9n);
+        let eleven: bigint[] = bigint_to_array(4, 2, 11n);
+        let fourteen: bigint[] = bigint_to_array(4, 2, 14n);
+        let thirteen: bigint[] = bigint_to_array(4, 2, 13n);
+
         let input: bigint[][][];
         let output: bigint[][][];
         if (x == 0) {
             input = [[one, zero], [zero, zero], [zero, zero], [zero, zero], [zero, zero], [zero, zero]];
-            output = [[one, zero], [zero, zero], [zero, zero], [zero, zero], [zero, zero], [zero, zero]]
+            output = [[one, zero], [zero, zero], [zero, zero], [zero, zero], [zero, zero], [zero, zero]];
+        }
+        if (x == 1) {
+            input = [[zero, zero], [zero, zero], [one, zero], [zero, zero], [zero, zero], [zero, zero]];
+            output = [[zero, zero], [zero, zero], [zero, zero], [zero, zero], [nine, eight], [zero, zero]];
+        }
+        if (x == 2) {
+            input = [[zero, zero], [zero, zero], [one, zero], [zero, one], [zero, one], [zero, zero]];
+            output = [[fourteen, eleven], [seven, seven], [one, thirteen], [three, nine], [fourteen, four], [thirteen, zero]];
         }
 
         it('Testing case: ' + x, async function() {
